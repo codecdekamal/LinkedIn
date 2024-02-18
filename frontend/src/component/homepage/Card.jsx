3dcd3a9e150f858faf4860630de9630b1a8ea2b2
kamal <kamalkj9368@gmail.com>
2024-02-18 18:43:10 +0530

    adding readux

--- frontend/src/component/homepage/Card.jsx
+++ frontend/src/component/homepage/Card.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Card = ({children}) => {
   return (
     <div >
-         <div className='border-gray-400 border-[0.5px]  border-t border-b sm:border-2 min-h-60 min-w-full overflow-hidden  sm:rounded-xl sm:p-2  mt-14 mb-28'>
+         <div className='border-gray-400 border-[0.5px]  border-t border-b sm:border-2 min-h-60 min-w-full overflow-hidden  sm:rounded-xl sm:p-2  mt-14  mb-28'>
         {children}
     </div> 
     </div>
--- frontend/src/component/homepage/SocialIcon.jsx
+++ frontend/src/component/homepage/SocialIcon.jsx
@@ -1,23 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faShare, faRepeat, faThumbsUp, faComment } from "./Icons";
 
 const SocialIcon = () => {
   const IconLists = [
-    { icon: faComment },
-    { icon: faRepeat },
-    { icon: faShare },
-    { icon: faThumbsUp },
+    { icon: faThumbsUp, name: "like", num: 0 },
+    { icon: faComment, name: "comment", num: 0 },
+    { icon: faRepeat, name: "repeat", num: 0 },
+    { icon: faShare, name: "share" },
   ];
-const onClickEventHandler =() =>{
+  const [changeNum,setChangeNum] = useState(IconLists);
 
-}
+  const onClickEventHandler = (name) => {
+  const gettingObj =   changeNum.find((icons)=>{
+         return icons.name === name
+    })
+    gettingObj.num+1;
+    console.log(gettingObj);
+    //  setChangeNum((prev)=>[...prev,{...gettingObj,num:num+=1}])
+    //  console.log(changeNum)
+  };
   return (
     <div className="flex flex-row justify-between px-10">
       {IconLists.map((icons) => {
-       return <button onClick={onClickEventHandler} className="text-gray-500 text-2xl">
+        return (
+          <button
+            key={icons.name}
+            onClick={() => onClickEventHandler(icons.name)}
+            className="text-2xl hover:text-gray-600 text-blue-400"
+          >
+            <span className="text-lg px-1 text-blue-500">{icons.num}</span>
             <FontAwesomeIcon icon={icons.icon}></FontAwesomeIcon>
           </button>
+        );
       })}
     </div>
   );
